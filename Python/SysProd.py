c38881d61ad3c2ec7bde8e6e80b4211159031e17
Stefan von Buddenbrock <stefvonb@gmail.com>
2018-03-15 16:54:54 +0200

    started config parsing, need to still update logging

--- Python/SysProd.py
+++ Python/SysProd.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python
 
+from ConfigParser import SafeConfigParser
+from argparse import ArgumentParser
+import Utilities
+from datetime import datetime
+
+parser = ArgumentParser()
+parser.add_argument(dest='config_file', help="The input config file", type=str)
+parser.add_argument('--log', help="The output log file name", type=str,
+                    default=datetime.now().strftime("log_%Y-%m-%d_%H:%M:%S.txt"))
+args = parser.parse_args()
+
+log = Utilities.Logger
+#log.info('jknasd')
+
+top_config = SafeConfigParser()
+top_config.readfp(open(args.config_file))
+
+print args.log
+
+for s in top_config.sections():
+    for i in top_config.items(s):
+        print i[0], i[1]
