d1507854eadaec0312bf2f5f756a8c05ed404dc9
Stefan von Buddenbrock <stef.von.b@cern.ch>
2018-03-15 23:47:02 +0200

    small progress

--- Python/SysProd.py
+++ Python/SysProd.py
@@ -4,6 +4,7 @@ from ConfigParser import SafeConfigParser
 from argparse import ArgumentParser
 import Utilities
 from datetime import datetime
+import pprint
 
 parser = ArgumentParser()
 parser.add_argument(dest='config_file', help="The input config file", type=str)
@@ -11,14 +12,7 @@ parser.add_argument('--log', help="The output log file name", type=str,
                     default=datetime.now().strftime("log_%Y-%m-%d_%H:%M:%S.txt"))
 args = parser.parse_args()
 
-log = Utilities.Logger
-#log.info('jknasd')
+top_config = Utilities.configDict(args.config_file, tokenize=['categories', 'observables'])
+np_config = Utilities.configDict(top_config['main']['NPlist'])
 
-top_config = SafeConfigParser()
-top_config.readfp(open(args.config_file))
 
-print args.log
-
-for s in top_config.sections():
-    for i in top_config.items(s):
-        print i[0], i[1]
--- Python/Utilities.py
+++ Python/Utilities.py
@@ -1,5 +1,22 @@
-import logging
+from ConfigParser import SafeConfigParser
 
-
-class Logger(logging):
-    pass
+def configDict(filename, tokenize=None):
+    """
+    A simple way to get a dictionary from a config file.
+    If a list of strings is passed in tokenize, then those items will be split
+    by commas and stripped of whitespace.
+    """
+    config = SafeConfigParser()
+    config.optionxform = str
+    with open(filename) as f:
+        config.readfp(f)
+        config_dict = config._sections
+        
+    for s, section in config_dict.iteritems():
+        del section['__name__']
+        if tokenize:
+            assert isinstance(tokenize, list)
+            for v, value in section.iteritems():
+                if ',' in value and v in tokenize:
+                    section[v] = [i.strip() for i in value.split(',')]
+    return config_dict
