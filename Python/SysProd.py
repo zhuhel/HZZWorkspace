c78f31bacf5609cec89722ff480251f995508474
Stefan von Buddenbrock <stefvonb@gmail.com>
2018-03-19 13:41:06 +0200

    started the iteration process

--- Python/SysProd.py
+++ Python/SysProd.py
@@ -3,17 +3,44 @@
 from argparse import ArgumentParser
 import Utilities
 import logging.config
+import sys, os
 
 parser = ArgumentParser()
 parser.add_argument(dest='config_file', help="The input config file", type=str)
+parser.add_argument('-o', '--output_dir', help="The output results directory", type=str, default='.')
 args = parser.parse_args()
 
-logging.config.fileConfig('logging.ini')
+script_loc = os.path.dirname(os.path.realpath(__file__))
+logging.config.fileConfig(script_loc + '/logging.ini')
+
+try:
+    import ROOT
+except ImportError:
+    logging.error("Could not import ROOT module. Make sure your root is configured to work with Python.")
+    sys.exit()
 
 logging.info("Calculating systematics from config file %s", args.config_file)
 
-top_config = Utilities.config_dict(args.config_file, tokenize=['categories', 'observables'])
+top_config = Utilities.config_dict(args.config_file, tokenize=['samples','categories', 'observables'])
 logging.debug(Utilities.config_display(top_config))
 np_config = Utilities.config_dict(top_config['main']['NPlist'])
 logging.debug(Utilities.config_display(np_config))
 
+if 'outdir' in top_config['main']:
+    logging.warn("Output directory %s found in config file, replacing the command line argument of %s",
+                 top_config['main']['outdir'], args.output_dir)
+
+if not Utilities.check_np(np_config):
+    logging.error("NP list file does not contain even 'up' and 'down' variations. Exiting...")
+    sys.exit()
+
+for sample in top_config['main']['samples']:
+    logging.info("Starting calculation for %s sample", sample)
+
+    for category in top_config['main']['categories']:
+        logging.info("Category: %s", category)
+        if "smooth" not in top_config[category]:
+            smooth = False
+            logging.info("Using binned histograms, since smoothing information not found in config file.")
+        else:
+            smooth = top_config[category]["smooth"]
--- Python/Utilities.py
+++ Python/Utilities.py
@@ -1,5 +1,6 @@
 from ConfigParser import SafeConfigParser
-import logging.config
+import logging
+import os
 
 
 def config_dict(filename, tokenize=None):
@@ -8,8 +9,6 @@ def config_dict(filename, tokenize=None):
     If a list of strings is passed in tokenize, then those items will be split
     by commas and stripped of whitespace.
     """
-    logging.config.fileConfig('logging.ini')
-
     config = SafeConfigParser()
     config.optionxform = str
     return_dict = None
@@ -21,7 +20,7 @@ def config_dict(filename, tokenize=None):
             return_dict = config._sections
             logging.info("Success!")
     except IOError:
-        logging.error("Could not find file %s", filename)
+        logging.error("Could not find file %s. Consider using absolute paths in your config file.", filename)
         raise
 
     if return_dict:
@@ -30,7 +29,7 @@ def config_dict(filename, tokenize=None):
             if tokenize:
                 assert isinstance(tokenize, list)
                 for v, value in section.iteritems():
-                    if ',' in value and v in tokenize:
+                    if v in tokenize:
                         section[v] = [i.strip() for i in value.split(',')]
         return return_dict
     else:
@@ -38,11 +37,36 @@ def config_dict(filename, tokenize=None):
 
 
 def config_display(cfg_dict):
+    """
+    A nice way to display the given config dictionary.
+    :param cfg_dict: the input config dictionary, as in the config_dict function above
+    :return: a formatted string with the sections and values
+    """
     return_str = "Listing config..."
     for s, section in cfg_dict.iteritems():
         return_str += "\n" + s + ":"
         for v, value in section.iteritems():
-           return_str += "\n--" + v + " = " + str(value)
-
+            return_str += "\n-- " + v + " = " + str(value)
     return return_str
 
+
+def check_np(np_dict):
+    """
+    Checks to see if the NP config file is appropriately designed.
+    :param np_dict: The config dict coming from the config_dict function above
+    :return: a boolean, True if the dict is fine, False if not
+    """
+    for s, section in np_dict.iteritems():
+        var_sum = 0
+        for v, value in section.iteritems():
+            if  "JET_JER_SINGLE_NP" in v:
+                continue
+            if "1up" in v:
+                var_sum += 1
+            elif "1down" in v:
+                var_sum -= 1
+
+        if var_sum == 0:
+            return True
+        else:
+            return False
