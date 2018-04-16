c1161b258fe74fe0c209d81d4f11d673b37267ea
svonbudd <stef.von.b@cern.ch>
2018-04-16 16:53:04 +0200

    added functionality and tweaks, including a prune factor

--- Python/modules/Plotter.py
+++ Python/modules/Plotter.py
@@ -3,7 +3,7 @@ import logging
 from array import array
 
 
-def plot_NPs(data, str_id, save_file=None):
+def plot_NPs(data, str_id, save_file=None, prune=0.01, publicity="Simulation Internal"):
     """
     Takes a dictionary with [NP][variation]["norm"], makes a pretty plot to print, and saves them to file optionally.
     """
@@ -15,6 +15,8 @@ def plot_NPs(data, str_id, save_file=None):
         # Store the percentage deviation
         up_variation = 100*(max([var['up']['norm'] - 1.0, var['down']['norm'] - 1.0, 0.0]))
         down_variation = 100*(min([var['up']['norm'] - 1.0, var['down']['norm'] - 1.0, 0.0]))
+        if up_variation - down_variation < prune:
+            continue
         plot_points.append((np.replace("ATLAS_", ""), down_variation, up_variation))
     
     # Sort by largest overall deviation
@@ -36,20 +38,21 @@ def plot_NPs(data, str_id, save_file=None):
 
     y_min, y_max = graph.GetYaxis().GetXmin(), graph.GetYaxis().GetXmax()
     line = ROOT.TLine(0.0, y_min, 0.0, y_max)    
-    line.SetLineStyle(3)
-    line.SetLineColor(14)
-    for i in range(n_points):      
-        line.DrawLine(float(i), y_min, float(i), 0.0)
+    line.SetLineColor(16)
+    for i in range(n_points):
+        line.DrawLine(float(i), y_min*1.025, float(i), plot_points[i][1])
 
     graph.GetXaxis().SetLabelSize(0)
     text = ROOT.TLatex()
     text.SetTextAlign(33)
     text.SetTextAngle(45)
     text.SetTextSize(0.02)
+    if len(plot_points) > 50:
+        text.SetTextSize(0.015)
     text.SetTextFont(42)
     for x, n in enumerate(plot_points):
         np = n[0] + " "
-        text.DrawLatex(x, y_min, np)
+        text.DrawLatex(x, y_min*1.05, np)
     
     graph.Draw("5")
 
@@ -57,13 +60,14 @@ def plot_NPs(data, str_id, save_file=None):
     legend.SetTextSize(0.035)
     legend.SetTextFont(42)
     legend.SetTextAlign(11)
-    legend.DrawLatexNDC(0.1, 0.91, "#bf{#it{ATLAS}} Simulation Internal")
+    legend.DrawLatexNDC(0.1, 0.91, "#bf{#it{ATLAS}} " + publicity)
     legend.SetTextAlign(31)
-    legend.DrawLatexNDC(0.9, 0.91, str_id)
+    legend.DrawLatexNDC(0.95, 0.91, str_id)
 
     canvas.SetGridy()
     canvas.SetBottomMargin(0.35)
     canvas.SetTopMargin(0.1)
+    canvas.SetRightMargin(0.05)
 
     canvas.Print(str_id + ".pdf")
     
