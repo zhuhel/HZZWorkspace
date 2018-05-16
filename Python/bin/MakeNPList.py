59a35602a875d9c561ecea2df2d05f9df3c233b2
svonbudd <stef.von.b@cern.ch>
2018-05-16 14:10:17 +0200

    started a script to make a NP list from a config file

--- Python/bin/MakeNPList.py
+++ Python/bin/MakeNPList.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-import Utilities
+import Utilities, sys, os, logging.config
 from argparse import ArgumentParser
-import logging.config
 
 parser = ArgumentParser()
 parser.add_argument(dest='config_file', help="The input config file", type=str)
@@ -10,3 +9,15 @@ args = parser.parse_args()
 
 script_loc = os.path.dirname(os.path.realpath(__file__))
 logging.config.fileConfig(script_loc + '/../configuration/logging_screen.ini')
+
+logging.info("Using config file %s to make nuisance parameter list", args.config_file)
+
+top_config = Utilities.config_dict(args.config_file, tokenize=[('samples', ',')])
+try:
+    np_list_filename = top_config['main']['NPlist']
+except KeyError:
+    logging.error("Please specify 'NPlist' in the config file to use as output filename and try again")
+    sys.exit()
+
+logging.info("Using output filename: %s", np_list_filename)
+
