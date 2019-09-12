34ca01383f5a96bd4cf865ed0b956b28d4a9144b
Hannah Elizabeth Herde <hannah.herde@cern.ch>
2019-09-12 12:17:39 +0200

    Update smooth.cxx
    Add comments

--- utils/smooth.cxx
+++ utils/smooth.cxx
@@ -1,6 +1,12 @@
 #include "HZZWorkspace/SmoothMan.h"
 
-int main(int argc, char **argv) 
+//------------------------------------------------------------------------------
+// Smooth histogram for given configuration.
+//
+// Examples in examples/Tools 
+//------------------------------------------------------------------------------
+
+int main(int argc, char **argv)
 {
     if (argc > 1 && string(argv[1]) == "help"){
         cout << argv[0] << " config_file " << endl;
@@ -10,7 +16,7 @@ int main(int argc, char **argv)
     if (argc > 1){
         config_name = string(argv[1]);
     }
-    
+
     SmoothMan *sm = new SmoothMan(config_name.c_str());
     sm->process();
     delete sm;
