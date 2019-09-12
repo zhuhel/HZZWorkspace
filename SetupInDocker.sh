21c519b2ac3886f132a576286638d7796d780bd9
Hannah Elizabeth Herde <hannah.herde@cern.ch>
2019-09-12 18:13:49 +0200

    Update SetupInDocker.sh

--- SetupInDocker.sh
+++ SetupInDocker.sh
@@ -1,7 +1,7 @@
 # Tell people what they need to set up and point to documentation
 
 echo "============================================="
-echo "You can safely ignore the above, "
+echo "You can safely ignore the above; "
 echo "this container is already fully set up"
 echo "to start using the Workspace framework. "
 echo
@@ -9,6 +9,7 @@ echo "For further documentation, please refer to  "
 echo "      https://gitlab.cern.ch/HZZ/HZZSoftware/HZZWorkspace/wikis/home"
 echo
 echo "You are in the HZZ build directory."
+echo "The code is mounted in the Docker volume as /hzz/"
 echo "Thank you for using HZZWorkspace with Docker! "
 echo "============================================="
 source /home/atlas/release_setup.sh
